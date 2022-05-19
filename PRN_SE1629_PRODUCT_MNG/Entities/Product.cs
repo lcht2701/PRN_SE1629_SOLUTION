b2a7472bb628a3044f5496a22fc76a85586028a4
thanhle22021 <86252279+thanhle22021@users.noreply.github.com>
2022-05-19 09:44:07 +0700

    commit product project management the first time

---
+++ PRN_SE1629_PRODUCT_MNG/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN.SE1629;
+    public class Product
+    {
+    /*1. Define properties */
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string Desc { get; set; }
+    public int UnitPrice { get; set; }
+    public int Quantity { get; set; }
+
+    public Product()
+    {
+    }
+
+    public Product(int id, string productName, string desc, int unitPrice, int quantity)
+    {
+        Id = id;
+        ProductName = productName;
+        Desc = desc;
+        UnitPrice = unitPrice;
+        Quantity = quantity;
+    }
+    //Expression-Bodied
+    public override string? ToString() => $"[Id = {Id}, Product Name = {ProductName}, Desc = {Desc}, Unit Price = {UnitPrice} and Quantity = {Quantity}]";
+
+    /*increase percent unit_price*/
+    public void IncreaseUnitPrice(int unitPrice) => UnitPrice = UnitPrice * unitPrice / 100;
+
+}
